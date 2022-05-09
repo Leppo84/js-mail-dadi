1287b86bdf347eadd842c2f7c2868ea8977e83e5
LeppoXena <francesco.lepore@xenagames.com>
2022-05-09 16:52:22 +0200

    Mail check adxded, script to be fixed

--- mail/myscript.js
+++ mail/myscript.js
@@ -1,1 +1,23 @@
-console.log("debug")
+// creo un elenco mail e lo rendo un array
+
+const mailingList = ["goofy@yahoo.it", "mickey.mouse@gmail.it", "minnie@tiscali.it", "tap@gmail.com", "tip@gmail.com"];
+console.log(mailingList)
+
+// faccio inserire la mail all'utente (form o prompt?)
+
+const userMail= prompt("inserire mail");
+console.log(userMail)
+
+// genero un ciclo per verificare se la mail è contenuta nell'elenco
+
+for (let index = 0; index < mailingList.length; index++) {
+    let element = mailingList[index];
+    if (element===userMail) {
+        document.getElementById("check_mail").innerHTML="Benvenuto/a in Topolinia mail!";
+        console.log(element);
+    }
+    else {
+        document.getElementById("check_mail").innerHTML="Ci dispiace, " + (userMail) + " non risulta essere una e-mail di Topolinia.";
+    }
+    console.log(element)
+}
